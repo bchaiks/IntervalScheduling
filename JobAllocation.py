3913406eb5bb7e0d521d191ce268cf41a068e4ff
Benjamin Chaiken <bfchaiken@gmail.com>
2025-04-03 08:25:12 -0700

    clean up comments

--- JobAllocation.py
+++ JobAllocation.py
@@ -8,16 +8,11 @@ import Scripts.Data as sd
 import Scripts.Optimization as opt
 import numpy as np
 import time
-import json
-import sys
 
 sFile = f"schedule.csv"
 rFile = f"machine_info.csv"
 
 
-'''
-NOTE: refactor this to just use python json library
-'''
 rs = sd.RawSchedule(sFile, rFile)
 
 # process raw data into input format
@@ -37,9 +32,6 @@ m.OptimizeSchedule()
 '''
 Need to refactor so that if this is infeasible, it returns
 a message that the current schedule is not feasible as given. 
-
-Probably would be nice to know if it's because 
-of adjacent rooms, or locked rooms, or something... 
 '''
 
 m.GetInitialPlan()
